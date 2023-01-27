4638b60f2ad7674a061d935321ed1ddc94e3df74
anuj123-45 <anuj1358.be20@chitkara.edu.in>
2023-01-27 12:11:35 +0530

    changes done

--- Grocery Store/src/checkout.js
+++ Grocery Store/src/checkout.js
@@ -19,7 +19,7 @@ export default function FormValidation() {
     localStorage.setItem("Customer Details", JSON.stringify(a));
     const { firstName, lastName, email, phone, address } = data;
     console.log(firstName, lastName, email, phone, address);
-    fetch("http://localhost:5000/register", {
+    fetch("https://amazing-supermarket.netlify.app/register", {
       method: "POST",
       crossDomain: true,
       headers: {
